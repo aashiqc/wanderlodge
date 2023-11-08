94535c1ea026e1dfb8e0b6c7a7e9b67c7ff2e263
Ashiq <muhmdashiqc@gmail.com>
2023-11-08 23:40:14 +0530

    listings updated

--- app/components/Navbar/Navbar.tsx
+++ app/components/Navbar/Navbar.tsx
@@ -18,7 +18,7 @@ const Navbar: React.FC<NavbarProps> = ({
   currentUser
 }) => {
   return (
-    <div className='fixed w-full  shadow-sm'>
+    <div className='fixed w-full bg-white z-10 shadow-sm'>
       <div className='py-4 border-b-[1px]'>
            <Container>
             <div className="flex flex-grow items-center justify-between gap-3 md:gap-0">
